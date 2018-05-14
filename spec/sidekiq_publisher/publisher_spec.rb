39b0d9352fc251f13add173f388e576f7377ee8e
Tim Perkins <tim.perkins@ezcater.com>
2018-05-14 16:08:11 -0400

    Rubocop

--- spec/sidekiq_publisher/publisher_spec.rb
+++ spec/sidekiq_publisher/publisher_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 RSpec.describe SidekiqPublisher::Publisher do
   let(:client) { instance_double(SidekiqPublisher::Client) }
   let(:publisher) { described_class.new }
