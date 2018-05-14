2a495cfffe24892b49f7d9e8fd15fce2b2e753ce
Tim Perkins <tjwp@users.noreply.github.com>
2018-05-14 16:10:10 -0400

    Add client for heterogeneous bulk publish (#5)

---
+++ lib/sidekiq_publisher/client.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+require "sidekiq"
+
+module SidekiqPublisher
+  class Client < Sidekiq::Client
+    def bulk_push(items)
+      payloads = items.map do |item|
+        normed = normalize_item(item)
+        process_single(item["class"], normed)
+      end.compact
+
+      pushed = 0
+      with_connection do |conn|
+        conn.multi do
+          payloads.each do |payload|
+            atomic_push(conn, [payload])
+            pushed += 1
+          end
+        end
+      end
+
+      pushed
+    end
+
+    private
+
+    def with_connection(&blk)
+      @redis_pool.with(&blk)
+    end
+  end
+end
