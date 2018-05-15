d161cff999eee0e76ff625840a5da9c5c69cce48
Tim Perkins <tim.perkins@ezcater.com>
2018-05-15 08:35:39 -0400

    Add Publisher class (#6)

---
+++ lib/sidekiq_publisher/publisher.rb
@@ -0,0 +1,68 @@
+# frozen_string_literal: true
+
+require "sidekiq_publisher/client"
+
+module SidekiqPublisher
+  class Publisher
+    extend PrivateAttr
+
+    private_attr_reader :client
+
+    def initialize
+      @client = SidekiqPublisher::Client.new
+    end
+
+    def publish
+      Job.unpublished_batches do |batch|
+        items = batch.map do |job|
+          {
+            "jid" => job[:job_id],
+            "class" => job[:job_class].constantize,
+            "args" => job[:args],
+            "at" => job[:run_at],
+            "queue" => job[:queue],
+            "wrapped" => job[:wrapped],
+          }.tap(&:compact!)
+        end
+
+        publish_batch(batch, items)
+      end
+      purge_expired_published_jobs
+    rescue StandardError => ex
+      failure_warning(__method__, ex)
+    end
+
+    private
+
+    def publish_batch(batch, items)
+      pushed_count = client.bulk_push(items)
+      published_count = update_jobs_as_published!(batch)
+    rescue StandardError => ex
+      failure_warning(__method__, ex)
+    ensure
+      update_jobs_as_published!(batch) if pushed_count.present? && published_count.nil?
+    end
+
+    def update_jobs_as_published!(jobs)
+      Job.published!(jobs.map { |job| job[:id] })
+    end
+
+    def purge_expired_published_jobs
+      Job.purge_expired_published! if perform_purge?
+    end
+
+    def perform_purge?
+      rand(100).zero?
+    end
+
+    def failure_warning(method, ex)
+      logger.warn("#{self.class.name}: msg=\"#{method} failed\" error=#{ex.class} error_msg=#{ex.message.inspect}\n"\
+                  "#{ex.backtrace.join("\n")}")
+      SidekiqPublisher.exception_reporter&.call(ex)
+    end
+
+    def logger
+      SidekiqPublisher.logger
+    end
+  end
+end
