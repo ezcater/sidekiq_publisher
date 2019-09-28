accbdb8da2c33fd57f11d5f37a43ebf7b6d63b1a
Tim Perkins <tim.perkins@ezcater.com>
2019-09-28 19:42:07 -0400

    Preserve Sidekiq::Worker.client_push

--- lib/sidekiq_publisher/worker.rb
+++ lib/sidekiq_publisher/worker.rb
@@ -4,6 +4,7 @@ module SidekiqPublisher
   module Worker
     def self.included(base)
       base.include(Sidekiq::Worker)
+      base.singleton_class.public_send(:alias_method, :sidekiq_client_push, :client_push)
       base.extend(ClassMethods)
     end
 
