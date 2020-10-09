c9623f1ce18b268b6ac693ed404bf300566c6946
Tony Vec <tvec623@gmail.com>
2020-10-09 11:55:01 -0400

    Address codeclimate complaints
    Move error type check into its own thing

--- lib/sidekiq_publisher/report_unpublished_count.rb
+++ lib/sidekiq_publisher/report_unpublished_count.rb
@@ -9,9 +9,14 @@ module SidekiqPublisher
       instrumenter.instrument("unpublished.reporter",
                               unpublished_count: SidekiqPublisher::Job.unpublished.count)
     rescue ActiveRecord::StatementInvalid, PG::UnableToSend, PG::ConnectionBad => e
-      cause = e.is_a?(ActiveRecord::StatementInvalid) ? e.cause : e
-      ActiveRecord::Base.clear_active_connections! if cause.is_a?(PG::UnableToSend) || cause.is_a?(PG::ConnectionBad)
+      ActiveRecord::Base.clear_active_connections! if db_connection_error?(e)
       raise
     end
+
+    def self.db_connection_error?(error)
+      cause = error.is_a?(ActiveRecord::StatementInvalid) ? error.cause : error
+      cause.is_a?(PG::UnableToSend) || cause.is_a?(PG::ConnectionBad)
+    end
+    private_class_method :db_connection_error?
   end
 end
