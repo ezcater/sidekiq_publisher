d47749ba344059bda81d287dbefb675e621fea21
Andy Rusterholz <andy.rusterholz@ezcater.com>
2022-05-20 15:11:24 -0600

    update rubocop

--- spec/factories/sidekiq_publisher_jobs.rb
+++ spec/factories/sidekiq_publisher_jobs.rb
@@ -1,10 +1,7 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
-  factory :sidekiq_publisher_job,
-          class: SidekiqPublisher::Job,
-          aliases: %i(publisher_job unpublished_job) do
-
+  factory :sidekiq_publisher_job, class: SidekiqPublisher::Job, aliases: %i(publisher_job unpublished_job) do
     job_id { SidekiqPublisher::Job.generate_sidekiq_jid }
     job_class { "TestJobClass" }
     sequence(:args) { |n| Hash[x: n] }
