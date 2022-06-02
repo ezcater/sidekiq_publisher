79c3b3671e3c13f42ce1ecd017a2f147abba8285
Andy Rusterholz <andy.rusterholz@ezcater.com>
2022-06-02 10:37:00 -0600

    update client to not use process_single

--- lib/sidekiq_publisher/client.rb
+++ lib/sidekiq_publisher/client.rb
@@ -7,7 +7,7 @@ module SidekiqPublisher
     def bulk_push(items)
       payloads = items.map do |item|
         normed = normalize_item(item)
-        process_single(item["class"], normed) || nil
+        middleware.invoke(item["class"], normed, normed["queue"], @redis_pool) { normed } || nil
       end.compact
 
       pushed = 0
